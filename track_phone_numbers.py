b1a421d1d6852377d99bc407f391aa5858fd70cb
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2023-08-28 22:19:34 +0530

    Update track_phone_numbers.py

--- track_phone_numbers.py
+++ track_phone_numbers.py
@@ -1,1 +1,15 @@
+import phonenumbers
+from phonenumbers import geocoder
+phone_number1 = phonenumbers.parse("+94701404652")
+phone_number2 = phonenumbers.parse("+94763441171")
+phone_number3 = phonenumbers.parse("+94716886067")
+phone_number4 = phonenumbers.parse("+94712324082")
+
+print("\nPhone Numbers Location\n")
+print(geocoder.description_for_number(phone_number1,"en"));
+print(geocoder.description_for_number(phone_number2,"en"));
+print(geocoder.description_for_number(phone_number3,"en"));
+print(geocoder.description_for_number(phone_number4,"en"));
+
+#LETS TRACK PHONE NUMBERS
 
